02dec0b9600656e34cc1ecd0772ed8148a2e7ca0
Sopan Chavan <sopanc542@gmail.com>
2026-09-20 17:27:47 +0530

    Initial Commit

---
+++ src/main/java/lost_and_found/controller/HomeController.java
@@ -0,0 +1,13 @@
+package lost_and_found.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/test/java/lost_and_found/LostAndFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package lost_and_found;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
