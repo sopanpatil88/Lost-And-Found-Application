4454228b920dc5b93da1bfc214b3ac1933fa579b
Sopan Chavan <sopanc542@gmail.com>
2026-09-21 13:16:39 +0530

    Login Page Inserted

--- src/main/java/lost_and_found/controller/HomeController.java
+++ src/main/java/lost_and_found/controller/HomeController.java
@@ -10,4 +10,10 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login() {
+    return "login";
+}
+    
 }
